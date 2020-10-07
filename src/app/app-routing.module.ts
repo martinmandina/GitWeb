09fb18abd87af1778fe3ce26766e329158d8e6fb
martinsmandina <martinsmandina@gmail.com>
2020-10-07 20:15:05 +0300

    import routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { WebComponent } from './web/web.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+    { path: 'web', component: WebComponent},
+    { path: 'Repository', component: FormComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
